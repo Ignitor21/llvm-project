1d9368f0aa93e67abce9c4f418c42bbb92ba69eb
George Samoylov <Ignit218@yandex.ru>
2026-03-24 21:27:55 +0300

    [GogiPC] 14. Update GogiPCRegisterInfo and add to GogiPCSubtarget

--- llvm/lib/Target/GogiPC/GogiPCRegisterInfo.cpp
+++ llvm/lib/Target/GogiPC/GogiPCRegisterInfo.cpp
@@ -11,3 +11,66 @@ using namespace llvm;
 GogiPCRegisterInfo::GogiPCRegisterInfo() : GogiPCGenRegisterInfo(GogiPC::R0) {
   GOGIPC_DUMP_GREEN
 }
+
+const MCPhysReg *
+GogiPCRegisterInfo::getCalleeSavedRegs(const MachineFunction *MF) const {
+  GOGIPC_DUMP_GREEN
+  return CSR_GogiPC_SaveList;
+}
+
+BitVector GogiPCRegisterInfo::getReservedRegs(const MachineFunction &MF) const {
+  GOGIPC_DUMP_GREEN
+  GogiPCFrameLowering const *TFI = getFrameLowering(MF);
+
+  BitVector Reserved(getNumRegs());
+  Reserved.set(GogiPC::R1);
+
+  if (TFI->hasFP(MF)) {
+    Reserved.set(GogiPC::R2);
+  }
+  return Reserved;
+}
+
+bool GogiPCRegisterInfo::requiresRegisterScavenging(
+    const MachineFunction &MF) const {
+  return false;
+}
+
+bool GogiPCRegisterInfo::eliminateFrameIndex(MachineBasicBlock::iterator II,
+                                          int SPAdj, unsigned FIOperandNum,
+                                          RegScavenger *RS) const {
+  GOGIPC_DUMP_GREEN
+  assert(SPAdj == 0 && "Unexpected non-zero SPAdj value");
+
+  MachineInstr &MI = *II;
+  MachineFunction &MF = *MI.getParent()->getParent();
+  DebugLoc DL = MI.getDebugLoc();
+
+  int FrameIndex = MI.getOperand(FIOperandNum).getIndex();
+  Register FrameReg;
+  int Offset = getFrameLowering(MF)
+                   ->getFrameIndexReference(MF, FrameIndex, FrameReg)
+                   .getFixed();
+  Offset += MI.getOperand(FIOperandNum + 1).getImm();
+
+  if (!isInt<16>(Offset)) {
+    llvm_unreachable("");
+  }
+
+  MI.getOperand(FIOperandNum).ChangeToRegister(FrameReg, false, false, false);
+  MI.getOperand(FIOperandNum + 1).ChangeToImmediate(Offset);
+  return false;
+}
+
+Register GogiPCRegisterInfo::getFrameRegister(const MachineFunction &MF) const {
+  GOGIPC_DUMP_GREEN
+  const TargetFrameLowering *TFI = getFrameLowering(MF);
+  return TFI->hasFP(MF) ? GogiPC::R2 : GogiPC::R1;
+}
+
+const uint32_t *
+GogiPCRegisterInfo::getCallPreservedMask(const MachineFunction &MF,
+                                      CallingConv::ID CC) const {
+  GOGIPC_DUMP_GREEN
+  return CSR_GogiPC_RegMask;
+}
--- llvm/lib/Target/GogiPC/GogiPCSubtarget.h
+++ llvm/lib/Target/GogiPC/GogiPCSubtarget.h
@@ -4,6 +4,7 @@
 #include "GogiPC.h"
 #include "GogiPCFrameLowering.h"
 #include "GogiPCISelLowering.h"
+#include "GogiPCRegisterInfo.h"
 #include "llvm/CodeGen/TargetSubtargetInfo.h"
 
 #define GET_SUBTARGETINFO_HEADER
@@ -14,6 +15,7 @@ namespace llvm {
 class GogiPCSubtarget : public GogiPCGenSubtargetInfo {
   GogiPCTargetLowering TLInfo;
   GogiPCFrameLowering FrameLowering;
+  GogiPCRegisterInfo RegInfo;
 
 public:
   GogiPCSubtarget(const Triple &TT, const std::string &CPU, const std::string &FS,
@@ -30,6 +32,10 @@ public:
   const GogiPCFrameLowering *getFrameLowering() const override {
     GOGIPC_DUMP_CYAN
     return &FrameLowering;
+  }
+  const GogiPCRegisterInfo *getRegisterInfo() const override {
+    GOGIPC_DUMP_CYAN
+    return &RegInfo;
   }
 };
 
